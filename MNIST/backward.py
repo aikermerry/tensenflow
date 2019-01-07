7594abd71b46632e74b5632601ecf7d231abfbd3
aikermerry <1139513692@qq.com>
2019-01-07 08:39:49 +0800

    mnist

--- MNIST/backward.py
+++ MNIST/backward.py
@@ -8,8 +8,8 @@ import os
 
 STEPS = 40000
 BATCH_SIZE = 200
-LEARNING_RATE_BASE=0.1
-LEARNING_RATE_DECAY=0.99
+LEARNING_RATE_BASE=0.001
+LEARNING_RATE_DECAY=0.9999
 MOVING_AVERAGE_DECAY =0.99
 regularizer = 0.0001
 mode_path = "./model"
@@ -20,7 +20,7 @@ def backward(mnist):
     y_ = tf.placeholder(tf.float32,shape=(None,forward.OUT_NODE))
 
     y,w1,w2 = forward.forward(x,regularizer)
-    global_step = tf.Variable(tf.constant(38000),trainable=False)
+    global_step = tf.Variable(tf.constant(0),trainable=False)
 #定义损失函数
     #loss_mse = tf.reduce_mean(tf.square(y-y_))
     #loss_total = loss_mse+tf.add_n(tf.get_collection('losses'))
@@ -47,8 +47,8 @@ def backward(mnist):
     with tf.control_dependencies([train_step,ema_op]):
         train_op = tf.no_op(name = 'train')
     
-    saver = tf.train.Saver()
-
+    saver = tf.train.Saver(max_to_keep=1)
+   
     with tf.Session() as sess:
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
@@ -56,22 +56,26 @@ def backward(mnist):
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess,ckpt.model_checkpoint_path) 
 
-        for i in range(40000):
-            xs,ys = mnist.train.next_batch(BATCH_SIZE)
+        for i in range(40000000):
+            try:
+                xs,ys = mnist.train.next_batch(BATCH_SIZE)
 
-            #start = ((i*BATCH_SIZE)+BATCH_SIZE)%data_size
-            #end = min(start+BATCH_SIZE,data_size)
-            _,loss_value,step=sess.run([train_op,loss_total,global_step],feed_dict={x:xs ,y_:ys})
+                #start = ((i*BATCH_SIZE)+BATCH_SIZE)%data_size
+                #end = min(start+BATCH_SIZE,data_size)
+                _,loss_value,step=sess.run([train_op,loss_total,global_step],feed_dict={x:xs ,y_:ys})
 
-            if i%2000 ==0 :
-                print(str(sess.run(y,feed_dict={x:xs ,y_:ys})))
-                print("--"*20)
+                if i%2000 ==0 :
+                    #print(str(sess.run(y,feed_dict={x:xs ,y_:ys})))
+                    print("--"*20)
 
-                print("steps:"+str(step)+" 损失："+str(loss_value))
-                #保存
+                    print("steps:"+str(step)+" 损失："+str(loss_value))
+                    #保存
+                    saver.save(sess,os.path.join(mode_path,mode_name),global_step=global_step)
+            except Exception as f:
+                print("不定义捕获的异常是：%s"%f)
+            finally:
                 saver.save(sess,os.path.join(mode_path,mode_name),global_step=global_step)
-
-       
+           
               
 
 if __name__ == "__main__":
--- MNIST/test.py
+++ MNIST/test.py
@@ -4,15 +4,16 @@ from tensorflow.examples.tutorials.mnist import input_data
 import forward
 import backward
 
+TEST_INTERVAL_SECS = 10
 
-
-def test():
+def test(mnist):
 
     with tf.Graph().as_default() as g:
 
         #定义x y_ y
         x = tf.placeholder(tf.float32,shape=(None,forward.INPUT_NODE))
-        y_ = tf.placeholder(tf.float32,shape=(None,forward.OUT_NODE))
+        y_ = tf.placeholder(tf.float32,shape=(None,forward.OUT_NODE))  #前向传播得到预测结果y
+        y,w1,w2  = forward.forward(x,None)  #前向传播得到y
 
 
         ema = tf.train.ExponentialMovingAverage(backward.MOVING_AVERAGE_DECAY)
@@ -20,26 +21,32 @@ def test():
         saver = tf.train.Saver(ema_restore)
 
 
-        correct_pre = tf.equal(tf.argmax(y,1),tf.argmax(y_1))
-        accuracy = tf.reduce_mean(tf.cost(correct_pre,tf.float32))
+        correct_pre = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pre,tf.float32))
         #实例化可还原滑动平均值的server
         #计算正确率
-        with tf.Session() as sess:
-         #加载模型 
-            ckpt = tf.train.get_checkpoint_state(backwward.mode_path)
+        while 1:
+            
+            with tf.Session() as sess:
+             #加载模型 
+                ckpt = tf.train.get_checkpoint_state(backward.mode_path)
 
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess,ckpt.model_checkpoint_path)
-                #恢复会话轮数
-                global_step = ckpt.model_checkpoint_split('/')[-1].split('-')[-1]
-                #计算准确率
-                accuracy_score = sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels})
-                print("after %s train steps,test accuracy = %g "%(global_step,accuracy_score))
-            else :
-                print("没有找到模型")
+                if ckpt and ckpt.model_checkpoint_path:
+                    
 
 
-if __name__== "__mian__":
+                    saver.restore(sess,ckpt.model_checkpoint_path)
+                    #恢复会话轮数
+                    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                    #计算准确率
+                    accuracy_score = sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels})
+                    print("after %s train steps,test accuracy = %g "%(global_step,accuracy_score))
+                else :
+                    print("没有找到模型")
+                time.sleep(TEST_INTERVAL_SECS)
+
+
+if __name__== "__main__":
 
     mnist=input_data.read_data_sets("/usr/local/lib/python3.6/dist-packages/tensorflow/contrib/learn/python/learn/datasets/data/",one_hot = True)
     test(mnist)
